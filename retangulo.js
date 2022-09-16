afaafa5096ea3b8abc53c189e24ff7fdc085011c
rodrigoaraujosantos <araujo.1983@hotmail.com>
2022-09-16 17:56:39 -0300

    retangulo

---
+++ retangulo.js
@@ -0,0 +1,8 @@
+alert('Retangulo no ar!')
+const base = Number(prompt('Entre com o tamanho da base!'))
+const altura = Number(prompt('Entre com a altura!'))
+const area = base * altura
+const perimetro = base + altura
+
+console.log(area)
+console.log(perimetro)
